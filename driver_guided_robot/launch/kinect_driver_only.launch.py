65778b57602f5e786fa5aa7011231adcf3151648
Aron Svastits <svastits.aron@gmail.com>
2021-08-04 14:32:04 +0200

    restructuring
    Splitting filter_points to utils, the actual filtering, and the system manager tasks

--- driver_guided_robot/launch/kinect_driver_only.launch.py
+++ driver_guided_robot/launch/kinect_driver_only.launch.py
@@ -13,112 +13,121 @@
 # limitations under the License.
 
 import os
-import yaml
-from launch import LaunchDescription
-import launch.actions
-from launch_ros.actions import Node
-import launch.substitutions
-import launch_ros.actions
 
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
 from launch.actions.include_launch_description import IncludeLaunchDescription
-from launch.launch_description_sources.python_launch_description_source import PythonLaunchDescriptionSource
+import launch_ros.actions
+from launch_ros.actions import Node
+import PythonLaunchDescriptionSource
+
+import yaml
 
 
 def load_file(package_name, file_path):
+
     package_path = get_package_share_directory(package_name)
     absolute_file_path = os.path.join(package_path, file_path)
 
     try:
         with open(absolute_file_path, 'r') as file:
             return file.read()
-    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
-        print("Couldnt load file " + absolute_file_path)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        print('Couldnt load file ' + absolute_file_path)
         return None
-        
+
+
 def load_yaml(package_name, file_path):
+
     package_path = get_package_share_directory(package_name)
     absolute_file_path = os.path.join(package_path, file_path)
 
     try:
         with open(absolute_file_path, 'r') as file:
             return yaml.safe_load(file)
-    except EnvironmentError: # parent of IOError, OSError *and* WindowsError where available
-        print("Couldnt load yaml " + absolute_file_path)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        print('Couldnt load yaml ' + absolute_file_path)
         return None
-        
+
+
 def generate_launch_description():
 
     # moveit_cpp.yaml is passed by filename for now since it's node specific
-    moveit_iiwa_yaml_file_name = get_package_share_directory('kinect_ros_trace_endpoint') + "/config/moveit_iiwa14.yaml"
-    
+    moveit_iiwa_yaml_file_name = get_package_share_directory(
+        'kinect_ros_trace_endpoint') + '/config/moveit_iiwa14.yaml'
+
     # Component yaml files are grouped in separate namespaces
     robot_description_config = load_file('urdflbriiwa14', 'urdf/urdflbriiwa14.urdf')
-    robot_description = {'robot_description' : robot_description_config}
-    
+    robot_description = {'robot_description': robot_description_config}
+
     robot_description_semantic_config = load_file('urdflbriiwa14', 'urdf/urdflbriiwa14.srdf')
-    robot_description_semantic = {'robot_description_semantic' : robot_description_semantic_config}
-    
+    robot_description_semantic = {'robot_description_semantic': robot_description_semantic_config}
+
     kinematics_yaml = load_yaml('kinect_ros_trace_endpoint', 'config/kinematics.yaml')
-    robot_description_kinematics = { 'robot_description_kinematics' : kinematics_yaml }
-    
-    ompl_planning_pipeline_config = { 'ompl' : {
-        'planning_plugin' : 'ompl_interface/OMPLPlanner',
-        'request_adapters' : """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds     default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""" ,
-        'start_state_max_bounds_error' : 0.1 } }
+
+    ompl_planning_pipeline_config = {'ompl': {
+        'planning_plugin': 'ompl_interface/OMPLPlanner',
+        'request_adapters': """default_planner_request_adapters/AddTimeOptimalParameterization
+        default_planner_request_adapters/FixWorkspaceBounds
+        default_planner_request_adapters/FixStartStateBounds
+        default_planner_request_adapters/FixStartStateCollision
+        default_planner_request_adapters/FixStartStatePathConstraints""",
+        'start_state_max_bounds_error': 0.1}}
     ompl_planning_yaml = load_yaml('kinect_ros_trace_endpoint', 'config/ompl_planning.yaml')
     ompl_planning_pipeline_config['ompl'].update(ompl_planning_yaml)
 
     trajectory_execution = {'moveit_manage_controllers': True,
-    
+
                             'trajectory_execution.allowed_execution_duration_scaling': 1.2,
                             'trajectory_execution.allowed_goal_duration_margin': 0.5,
                             'trajectory_execution.allowed_start_tolerance': 0.01}
-                            
+
     controllers_yaml = load_yaml('kinect_ros_trace_endpoint', 'config/fake_controllers.yaml')
     moveit_controllers = {'moveit_fake_controller_manager': controllers_yaml,
-                       'moveit_controller_manager': 'moveit_fake_controller_manager/MoveItFakeControllerManager'}
-                       
+                          'moveit_controller_manager':
+                              'moveit_fake_controller_manager/MoveItFakeControllerManager'}
+
     kinect_ros_trace_endpoint_node = Node(package='kinect_ros_trace_endpoint',
-                               executable='moveit_with_markerpos',
-                               name='moveit_with_markerpos',
-                               output='screen',
-                               arguments=['--ros-args', '--log-level', 'error'],                               
-                               remappings=[('fake_controller_joint_states', 'reference_joint_state')],
-                               parameters=[moveit_iiwa_yaml_file_name,
-                                           robot_description,
-                                           robot_description_semantic,
-                                           kinematics_yaml,
-                                           ompl_planning_pipeline_config,
-                                           trajectory_execution,
-                                           moveit_controllers])
+                                          executable='moveit_with_markerpos',
+                                          name='moveit_with_markerpos',
+                                          output='screen',
+                                          arguments=['--ros-args', '--log-level', 'error'],
+                                          remappings=[('fake_controller_joint_states',
+                                                       'reference_joint_state')],
+                                          parameters=[moveit_iiwa_yaml_file_name,
+                                                      robot_description,
+                                                      robot_description_semantic,
+                                                      kinematics_yaml,
+                                                      ompl_planning_pipeline_config,
+                                                      trajectory_execution,
+                                                      moveit_controllers])
 
     kuka_sunrise_dir = get_package_share_directory('kuka_sunrise')
 
     kuka_sunrise_interface = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([kuka_sunrise_dir, '/launch/kuka_sunrise.launch.py'])
         )
-        
+
     joint_controller = launch_ros.actions.LifecycleNode(
         package='robot_control', executable='joint_controller', output='screen',
         arguments=['--ros-args', '--log-level', 'info'],
-        name='joint_controller', remappings=[('measured_joint_state', 'lbr_joint_state'), ('joint_command', 'lbr_joint_command')]
+        name='joint_controller', remappings=[('measured_joint_state', 'lbr_joint_state'),
+                                             ('joint_command', 'lbr_joint_command')]
         )
-        
-       
+
     system_manager = launch_ros.actions.LifecycleNode(
         package='driver_guided_robot', executable='system_manager', output='screen',
         name='system_manager'
-        )        
+        )
 
-                                    
     joint_state_publisher = Node(package='joint_state_publisher',
                                  executable='joint_state_publisher',
                                  name='joint_state_publisher',
-                                 arguments=[get_package_share_directory('urdflbriiwa14') + "/urdf/urdflbriiwa14.urdf"],
+                                 arguments=[get_package_share_directory(
+                                     'urdflbriiwa14') + '/urdf/urdflbriiwa14.urdf'],
                                  output='log',
-                                 parameters=[{'source_list': ["/lbr_joint_state"], 'rate': 100}])
-        
+                                 parameters=[{'source_list': ['/lbr_joint_state'], 'rate': 100}])
+
     return LaunchDescription([
         kuka_sunrise_interface,
         system_manager,
--- filter_points/src/FilterPoints.cpp
+++ filter_points/src/FilterPoints.cpp
@@ -24,8 +24,7 @@ FilterPoints::FilterPoints(
   const rclcpp::NodeOptions & options)
 : rclcpp::Node(node_name, options), qos_(rclcpp::KeepLast(1))
 {
-  auto callback = [this](
-    visualization_msgs::msg::MarkerArray::SharedPtr msg) {
+  auto callback = [this](visualization_msgs::msg::MarkerArray::SharedPtr msg) {
       this->markersReceivedCallback(msg);
     };
   marker_listener_ = this->create_subscription<
@@ -34,159 +33,22 @@ FilterPoints::FilterPoints(
     callback);
   goal_pos_publisher_ = this->create_publisher<geometry_msgs::msg::Pose>(
     "goal_pos", qos_);
-  cbg_ = this->create_callback_group(
-    rclcpp::callback_group::CallbackGroupType::MutuallyExclusive);
-  get_state_client_ = this->create_client<
-    kuka_sunrise_interfaces::srv::GetState>(
-    "robot_control/get_fri_state");
-  this->declare_parameter(
-    "fake_execution",
-    rclcpp::ParameterValue(static_cast<bool>(false)));
+
+  auto stop_proc_callback = [this](
+    const std::shared_ptr<rmw_request_id_t> request_header,
+    std_srvs::srv::Trigger::Request::SharedPtr request,
+    std_srvs::srv::Trigger::Response::SharedPtr response) {
+      (void) request_header;
+      response->success = true;
+      rclcpp::shutdown();
+    };
+  stop_processing_service_ = this->create_service<std_srvs::srv::Trigger>(
+    "filter_points/stop_processing", stop_proc_callback);
+  change_state_client_ = this->create_client<std_srvs::srv::Trigger>(
+    "system_manager/trigger_change");
+
   prev_rel_pose_.position.x = prev_rel_pose_.position.y =
     prev_rel_pose_.position.z = 0;
-}
-
-void FilterPoints::CoordinateTransform(geometry_msgs::msg::Point & pos1)
-{
-  float tmp = pos1.x;
-  pos1.x = -pos1.z;
-  pos1.z = -pos1.y;
-  pos1.y = tmp;
-}
-
-geometry_msgs::msg::Quaternion FilterPoints::ToQuaternion(
-  const geometry_msgs::msg::Point & or1,
-  const geometry_msgs::msg::Point & or2,
-  const geometry_msgs::msg::Point & or3)
-{
-  geometry_msgs::msg::Quaternion result;
-  if (or1.x + or2.y + or3.z > 0) {
-    result.w = sqrt(1 + or1.x + or2.y + or3.z) / 2;
-    result.x = (or2.z - or3.y) / (4 * result.w);
-    result.y = (or3.x - or1.z) / (4 * result.w);
-    result.z = (or1.y - or2.x) / (4 * result.w);
-  } else if (or1.x > or2.y && or1.x > or3.z) {
-    float s = 2 * sqrt(1 + or1.x - or2.y - or3.z);
-    result.w = (or2.z - or3.y) / s;
-    result.x = s / 4;
-    result.y = (or1.y + or2.x) / s;
-    result.z = (or3.x + or1.z) / s;
-  } else if (or2.y > or3.z) {
-    float s = 2 * sqrt(1 - or1.x + or2.y - or3.z);
-    result.w = (or3.x - or1.z) / s;
-    result.x = (or1.y + or2.x) / s;
-    result.y = s / 4;
-    result.z = (or2.z + or3.y) / s;
-  } else {
-    float s = 2 * sqrt(1 - or1.x - or2.y + or3.z);
-    result.w = (or1.y - or2.x) / s;
-    result.x = (or3.x + or1.z) / s;
-    result.y = (or2.z + or3.y) / s;
-    result.z = s / 4;
-  }
-  float length = sqrt(
-    result.x * result.x + result.y * result.y + result.z * result.z +
-    result.w * result.w);
-  result.x /= length;
-  result.y /= length;
-  result.z /= length;
-  result.w /= length;
-
-  return result;
-}
-
-geometry_msgs::msg::Point FilterPoints::PoseDiff(
-  const geometry_msgs::msg::Point & pos1,
-  const geometry_msgs::msg::Point & pos2, bool coord_trans)
-{
-  geometry_msgs::msg::Point result;
-  result.x = pos1.x - pos2.x;
-  result.y = pos1.y - pos2.y;
-  result.z = pos1.z - pos2.z;
-
-  if (coord_trans) {
-    CoordinateTransform(result);
-  }
-
-  return result;
-}
-
-geometry_msgs::msg::Point FilterPoints::CrossProduct(
-  const geometry_msgs::msg::Point & pos1,
-  const geometry_msgs::msg::Point & pos2, bool normalize)
-{
-  geometry_msgs::msg::Point result;
-  result.x = pos1.y * pos2.z - pos1.z * pos2.y;
-  result.y = -pos1.x * pos2.z + pos1.z * pos2.x;
-  result.z = pos1.x * pos2.y - pos1.y * pos2.x;
-
-  if (normalize) {
-    float length = sqrt(
-      result.x * result.x + result.y * result.y +
-      result.z * result.z);
-    result.x /= length;
-    result.y /= length;
-    result.z /= length;
-  }
-
-  return result;
-}
-
-void FilterPoints::ChangeState(
-  const std::string & node_name,
-  std::uint8_t transition)
-{
-  if (!fake_execution_) {
-    auto client = this->create_client<lifecycle_msgs::srv::ChangeState>(
-      node_name + "/change_state", qos_.get_rmw_qos_profile(), cbg_);
-    auto request = std::make_shared<
-      lifecycle_msgs::srv::ChangeState::Request>();
-    request->transition.id = transition;
-    if (!client->wait_for_service(std::chrono::milliseconds(2000))) {
-      RCLCPP_ERROR(get_logger(), "Wait for service failed");
-    }
-    auto future_result = client->async_send_request(request);
-    auto future_status = kuka_sunrise::wait_for_result(
-      future_result,
-      std::chrono::milliseconds(3000));
-    if (future_status != std::future_status::ready) {
-      RCLCPP_ERROR(get_logger(), "Future status not ready");
-    }
-    if (!future_result.get()->success) {
-      RCLCPP_ERROR(get_logger(), "Future result not success");
-    }
-  } else {
-    RCLCPP_WARN(get_logger(), "Shutting down node in fake execution mode");
-    rclcpp::shutdown();
-  }
-}
-
-void FilterPoints::GetFRIState()
-{
-  while (!get_state_client_->wait_for_service(std::chrono::milliseconds(1000))) {
-    if (!rclcpp::ok()) {
-      RCLCPP_ERROR(
-        this->get_logger(),
-        "Interrupted while waiting for the service. Exiting.");
-      return;
-    }
-    RCLCPP_INFO(
-      this->get_logger(),
-      "service not available, waiting again...");
-  }
-  auto request = std::make_shared<
-    kuka_sunrise_interfaces::srv::GetState::Request>();
-
-  auto response_received_callback =
-    [this](
-    rclcpp::Client<kuka_sunrise_interfaces::srv::GetState>::SharedFuture future) {
-      auto result = future.get();
-      lbr_state_ = result->data;
-      RCLCPP_INFO(this->get_logger(), "State: %i", lbr_state_);
-    };
-  auto future_result = get_state_client_->async_send_request(
-    request,
-    response_received_callback);
 }
 
 void FilterPoints::markersReceivedCallback(
@@ -205,24 +67,21 @@ void FilterPoints::markersReceivedCallback(
     msg->markers.begin(), msg->markers.end(),
     [](const visualization_msgs::msg::Marker & marker) {
       int joint_id = marker.id % 100;
-      return joint_id ==
-      static_cast<int>(BODY_TRACKING_JOINTS::PELVIS);
+      return joint_id == static_cast<int>(BODY_TRACKING_JOINTS::PELVIS);
     });
 
   auto wrist_it = std::find_if(
     msg->markers.begin(), msg->markers.end(),
     [](const visualization_msgs::msg::Marker & marker) {
       int joint_id = marker.id % 100;
-      return joint_id ==
-      static_cast<int>(BODY_TRACKING_JOINTS::WRIST_RIGHT);
+      return joint_id == static_cast<int>(BODY_TRACKING_JOINTS::WRIST_RIGHT);
     });
 
   auto thumb_it = std::find_if(
     msg->markers.begin(), msg->markers.end(),
     [](const visualization_msgs::msg::Marker & marker) {
       int joint_id = marker.id % 100;
-      return joint_id ==
-      static_cast<int>(BODY_TRACKING_JOINTS::THUMB_RIGHT);
+      return joint_id == static_cast<int>(BODY_TRACKING_JOINTS::THUMB_RIGHT);
     });
 
   auto second_it = std::find_if(
@@ -236,15 +95,12 @@ void FilterPoints::markersReceivedCallback(
     msg->markers.begin(), msg->markers.end(),
     [](const visualization_msgs::msg::Marker & marker) {
       int joint_id = marker.id % 100;
-      return joint_id ==
-      static_cast<int>(BODY_TRACKING_JOINTS::HANDTIP_LEFT);
+      return joint_id == static_cast<int>(BODY_TRACKING_JOINTS::HANDTIP_LEFT);
     });
 
   if (second_it != msg->markers.end()) {
     RCLCPP_WARN(get_logger(), "Two bodies in view, deactivating control");
-    ChangeState(
-      "system_manager",
-      lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE);
+    change_state_client_->async_send_request(trigger_request_);
   }
 
   if (handtip_it != msg->markers.end() && thumb_it != msg->markers.end() &&
@@ -259,9 +115,7 @@ void FilterPoints::markersReceivedCallback(
       rel_pose_.position = PoseDiff(
         handtip_pose_.position,
         pelvis_pose_.position);
-      orientation_z_ = PoseDiff(
-        handtip_pose_.position,
-        wrist_pose_.position);
+      orientation_z_ = PoseDiff(handtip_pose_.position, wrist_pose_.position);
       orientation_y_ = PoseDiff(thumb_pose_.position, wrist_pose_.position);
       orientation_x_ = CrossProduct(orientation_y_, orientation_z_);
       orientation_y_ = CrossProduct(orientation_z_, orientation_x_);
@@ -274,71 +128,34 @@ void FilterPoints::markersReceivedCallback(
         left_stop_ = PoseDiff(stop_pose_.position, pelvis_pose_.position);
         if (left_stop_.z > 0.6) {
           RCLCPP_INFO(get_logger(), "Motion stopped with left hand");
-          ChangeState(
-            "system_manager",
-            lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE);
+          change_state_client_->async_send_request(trigger_request_);
         }
       } else {
         RCLCPP_WARN(
           get_logger(),
           "Left handtip joint not found, stopping not possible that way");
       }
-
-
-      // TODO(Svastits): missing error handling
-      this->get_parameter("fake_execution", fake_execution_);
-      if (!fake_execution_) {
-        GetFRIState();
-      }
-      if (4 == lbr_state_ || fake_execution_) {
-        stop_ = false;
-        auto delta = PoseDiff(
-          rel_pose_.position,
-          prev_rel_pose_.position);
-        float delta_len = sqrt(
-          delta.x * delta.x + delta.y * delta.y +
-          delta.z * delta.z);
-        if (delta_len > 0.1) {
-          goal_pos_publisher_->publish(rel_pose_);
-          prev_rel_pose_ = rel_pose_;
-          RCLCPP_INFO(get_logger(), "x: %f", rel_pose_.position.x);
-          RCLCPP_INFO(get_logger(), "y: %f", rel_pose_.position.y);
-          RCLCPP_INFO(get_logger(), "z: %f", rel_pose_.position.z);
-        } else {
-          RCLCPP_INFO(
-            get_logger(),
-            "Skipping frame, distance is only %f [cm]",
-            delta_len);
-        }
-
+      auto delta = PoseDiff(rel_pose_.position, prev_rel_pose_.position);
+      float delta_len = sqrt(
+        delta.x * delta.x + delta.y * delta.y + delta.z * delta.z);
+      if (delta_len > 0.1) {
+        goal_pos_publisher_->publish(rel_pose_);
+        prev_rel_pose_ = rel_pose_;
+        RCLCPP_INFO(get_logger(), "x: %f", rel_pose_.position.x);
+        RCLCPP_INFO(get_logger(), "y: %f", rel_pose_.position.y);
+        RCLCPP_INFO(get_logger(), "z: %f", rel_pose_.position.z);
       } else {
-        RCLCPP_ERROR(get_logger(), "FRI State is: %i", lbr_state_);
-
-        // Stop only if the state is not 4 for two consecutive cycles
-        // to avoid unnecessary shutdowns
-        if (!start_ && stop_) {
-          rclcpp::shutdown();
-        }
-        stop_ = true;
+        RCLCPP_INFO(
+          get_logger(), "Skipping frame, distance is only %f [cm]",
+          delta_len);
       }
     } else {
-      RCLCPP_WARN(
-        get_logger(),
-        "Pelvis joint not found, skipping frame");
-      ChangeState(
-        "system_manager",
-        lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE);
+      RCLCPP_WARN(get_logger(), "Pelvis joint not found, skipping frame");
+      change_state_client_->async_send_request(trigger_request_);
     }
   } else {
-    RCLCPP_WARN(
-      get_logger(),
-      "Right handtip joint not found, skipping frame");
-    ChangeState(
-      "system_manager",
-      lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE);
-  }
-  if (start_) {
-    start_ = false;
+    RCLCPP_WARN(get_logger(), "Right handtip joint not found, skipping frame");
+    change_state_client_->async_send_request(trigger_request_);
   }
 }
 }  // namespace filter_points
