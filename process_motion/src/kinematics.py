fab931fb6e35633c859a24bb21a2eddf731c68a6
Aron Svastits <svastits.aron@gmail.com>
2022-03-09 16:30:10 +0100

    [skip travis] changed orientation concept -> working servo

--- process_motion/src/kinematics.py
+++ process_motion/src/kinematics.py
@@ -48,19 +48,7 @@ def calc_jacobian(dh_params, joint_pos=None, orientation=True):
     """
 
     # Calculate base to end effector transform
-    T_full = np.identity(4)
-    rot_matrices = []
-    tool_length = 0
-    if 'tool_length' in dh_params.keys():
-        tool_length = dh_params['tool_length']
-    for i in range(joint_count):
-        joint_dh = dh_params[f'joint_{i + 1}'][0].split(' ')
-
-        if i == joint_count -1:
-            T_full = T_full * DH2T(*joint_dh, tool_length)
-        else:
-            T_full = T_full * DH2T(*joint_dh)
-        rot_matrices.append(T_full[:3, :3])
+    T_full = calc_transform(dh_params)
 
     # Calculate Jacobian from Transform matrix and rotations
     q = [sp.symbols(f'q{i + 1}') for i in range(joint_count)]
@@ -68,9 +56,19 @@ def calc_jacobian(dh_params, joint_pos=None, orientation=True):
     Jv = abs_pos.jacobian(q)
 
     if orientation:
-        Jw = sp.Matrix()
-        for i in range(len(rot_matrices)):
-            Jw = Jw.col_insert(i, rot_matrices[i]*sp.Matrix([0, 0, 1]))
+        R = T_full[:3, :3]
+        cp = sp.sqrt(R[0,0]**2 + R[1,0]**2) # this equals cos(pitch)
+               
+        yaw = sp.atan2(R[1,0], R[0,0])
+        pitch = sp.atan2(-R[2,0], cp)
+        roll = sp.atan2(R[2,1], R[2,2])
+
+        Jw = sp.Matrix([roll, pitch, yaw]).jacobian(q)
+
+        # This was different orientation
+        # Jw = sp.Matrix()
+        # for i in range(len(rot_matrices)):
+        #     Jw = Jw.col_insert(i, rot_matrices[i]*sp.Matrix([0, 0, 1]))
 
         J=sp.Matrix([Jv, Jw])
     else:
@@ -79,7 +77,20 @@ def calc_jacobian(dh_params, joint_pos=None, orientation=True):
     if joint_pos == None:
         return J
     else:
-        return J.subs(zip(q, joint_pos)).evalf(4)
+        if  orientation and cp.subs(zip(q, joint_pos)).evalf() < 1e-6:
+            js = joint_pos
+            print("Pitch is almost +-90°")
+            # in this case, the solution is not straighforward
+            # atan2(0, 0) would be nan -> orientation is calculated with a different joint setup
+            # only the first (from TCP) joint is modified, that changes pitch angle slightly
+            for i in reversed(range(joint_count)):
+                js[i] = joint_pos[i] + 0.001
+                if cp.subs(zip(q, js)).evalf() < 1e-6:
+                    js[i] = joint_pos[i]
+                else:
+                    break
+            print(js) 
+        return J.subs(zip(q, joint_pos)).evalf()
 
 
 def pseudo_inverse(J):
@@ -108,24 +119,34 @@ def servo_calcs(goal_pos, joint_states, orientation=True, max_iter=500):
     js = joint_states
     actual_pos = calc_forw_kin(calc_transform(dh_params), js)
     sp.pprint(actual_pos)
-    diff = sp.Matrix([goal_pos]).transpose() - sp.Matrix([actual_pos])
+    if orientation:
+        diff = sp.Matrix([goal_pos]).transpose() - sp.Matrix([actual_pos])
+    else:
+        diff = sp.Matrix([goal_pos[:3]]).transpose() - actual_pos[:3,:]
     i = 0
-    while (diff.norm() > 1e-3 or i < max_iter):
+    if diff.norm() > 1:
+        print("Initial distance too big")
+        return actual_pos, diff
+    while (diff.norm() > 1e-3 and i < max_iter):
+        print(diff.norm(), i)
         i += 1
-        J_inv = pseudo_inverse(calc_jacobian(dh_params, actual_pos, orientation))
+        J_inv = pseudo_inverse(calc_jacobian(dh_params, joint_states, orientation))
         if orientation:
             delta_theta = J_inv * diff
         else:
             print(diff[:3, :])
             delta_theta = J_inv * diff[:3, :]
         #delta_theta = delta_theta / max(delta_theta) * 0.02
-        new_joints = sp.Matrix(js) + delta_theta * 0.1
+        new_joints = sp.Matrix(js) + delta_theta * 0.3 # TODO: fine tune multiplier
+        # TODO: maybe split the cartesian difference into small distances
         js = [item for sublist in new_joints.tolist() for item in sublist]
         actual_pos = calc_forw_kin(calc_transform(dh_params), js)
         sp.pprint(actual_pos.transpose())
-        diff = sp.Matrix([goal_pos]).transpose() - sp.Matrix([actual_pos])
+        if orientation:
+            diff = sp.Matrix([goal_pos]).transpose() - sp.Matrix([actual_pos])
+        else:
+            diff = sp.Matrix([goal_pos[:3]]).transpose() - actual_pos[:3,:]
         sp.pprint(diff.transpose())
-        print(diff.norm())
 
     return js, diff
 
@@ -146,23 +167,29 @@ def calc_transform(dh_params):
 
 
 def calc_forw_kin(T, joint_pos):
+    js=joint_pos
     q = [sp.symbols(f'q{i + 1}') for i in range(joint_count)]
     abs_pos = T[:3, 3]
     R = T[:3, :3]
-    sy = sp.sqrt(R[0,0]**2 + R[1,0]**2)
-    if sy.subs(zip(q, joint_pos)).evalf() > 1e-6:
-        yaw = sp.atan2(R[1,0], R[0,0])
-        pitch = sp.atan2(-R[2,0], sy)
-        #pitch = sp.atan2(-R[2,0], sp.sqrt(R[2,1]**2 + R[2,2]**2))
-        roll = sp.atan2(R[2,1], R[2,2])
-    else:
-        roll = sp.atan2(-R[1,2], R[1,1])
-        pitch = sp.atan2(-R[2,0], sy)
-        yaw = sp.Matrix([0])
-    # TODO: RPY jacobian not the same
 
+    cp = sp.sqrt(R[0,0]**2 + R[1,0]**2) # this equals cos(pitch)
+    roll = sp.atan2(R[2,1], R[2,2])
+    pitch = sp.atan2(-R[2,0], cp)
+    yaw = sp.atan2(R[1,0], R[0,0])    
+    
+    if cp.subs(zip(q, js)).evalf() < 1e-6: 
+        print("Pitch is almost +-90°")
+        # in this case, the RPY angles are not straighforward
+        # atan2(0, 0) would be nan -> orientation is calculated with a slightly different joint setup
+        # only the first (from TCP) joint is modified, which changes pitch angle
+        for i in reversed(range(joint_count)):
+            js[i] = joint_pos[i] + 0.001
+            if cp.subs(zip(q, js)).evalf() < 1e-6:
+                js[i] = joint_pos[i]
+            else:
+                break
 
-    return sp.Matrix([abs_pos, roll, pitch, yaw]).subs(zip(q, joint_pos)).evalf(4)
+    return sp.Matrix([abs_pos, roll, pitch, yaw]).subs(zip(q, js)).evalf()
 
 
 
