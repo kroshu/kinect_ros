1e7b1db1087d75216a21031c2c945e90fe51591b
Aron Svastits <svastits.aron@gmail.com>
2022-03-30 09:26:04 +0200

    [skip travis] restructure tests + add benchmark tests

--- process_motion/src/test_kinematics.py
+++ process_motion/src/test_kinematics.py
@@ -14,13 +14,17 @@ import random
 import numpy as np
 import sympy as sp
 import yaml
+import pandas as pd
 
 import kinematics as kn
 
-SET_LAST = 0  # should equal the $tries argument of kinematic.adjust_goal function (odd integer or 0)
+SET_LAST = 5  # should equal the $tries argument of kinematic.adjust_goal function (odd integer or 0)
 
 CONFIG_PATH = os.path.join(str(Path(__file__).parent.parent.absolute()),
                            'config', 'LBR_iiwa_DH.yaml')
+
+CSV_PATH = os.path.join(str(Path(__file__).parent.parent.parent.parent.parent.absolute()),
+                           'data', 'set_test_constants', '0_combined.csv')
 
 with open(CONFIG_PATH, 'r', encoding="utf-8") as config_file:
     config_dict = yaml.safe_load(config_file)
@@ -46,6 +50,21 @@ def check_joint_limits(joint_states):
             return False
     return True
 
+def process_result(joint_result, success):
+    if joint_result == -1:
+        success.append(0)
+        servo_list = [np.nan] * 7
+    else:
+        for j in range(joint_count):
+            if abs(joint_result[j]) > sp.pi:
+                print('Joint value exceeds limit, but can be made valid')
+                cycles = int(abs(joint_result[j] / (2 * sp.pi)))
+                joint_result[j] -= np.sign(joint_result[j]) * 2 * sp.pi.evalf() * (cycles + 1)
+        success.append(1)
+        servo_list = [round(item, 4) for sublist in servo_joints.tolist() for item in sublist]
+
+    return servo_list
+
 
 # Print headers
 with open('test.csv', 'w', encoding="utf-8") as file:
@@ -57,57 +76,62 @@ with open('test.csv', 'w', encoding="utf-8") as file:
     writer.writerow(original + modified + servoed + results)
 
 
-for i in range(500):
-    print(f'{i + 1}. iteration')
-    js_orig = []  # this should be reached
-    diff = []
-    joint_states = []  # this is measured by the camera
+i = 0
+data_csv = pd.read_csv(CSV_PATH, sep=',', decimal='.')
+while i < 500:   
     success = []
     distances = []
-    for j in range(joint_count):
-        # Specific joint limits for iiwa:
-        if j in [1, 3, 5]:
-            js_orig.append(round(random.uniform(math.radians(-120), math.radians(120)), 3))
-        else:
-            js_orig.append(round(random.uniform(math.radians(-170), math.radians(170)), 3))
+    diff = []
+    print(f'{i + 1}. iteration')
+    # Run pre-defined test cases
+    if i < data_csv.shape[0]:
+        js_orig = data_csv.iloc[i][:7].values.tolist()
+        joint_states = data_csv.iloc[i][7:].values.tolist()
+        zip_object = zip(joint_states, js_orig)
+        for list1_i, list2_i in zip_object:
+            diff.append(list1_i-list2_i)
+    else:
+        js_orig = []  # this should be reached
+        joint_states = []  # this is measured by the camera
 
-        # Slight changes in joints based on experience
-        if j in range(4):
-            diff.append(round(random.uniform(-0.2, 0.2), 3))
-        else:
-            diff.append(round(random.uniform(-0.5, 0.5), 3))
-    for j in range(joint_count):
-        joint_states.append(js_orig[j] + diff[j])
+        for j in range(joint_count):
+            # Specific joint limits for iiwa:
+            if j in [1, 3, 5]:
+                js_orig.append(round(random.uniform(math.radians(-120), math.radians(120)), 3))
+            else:
+                js_orig.append(round(random.uniform(math.radians(-170), math.radians(170)), 3))
 
-    # js_orig = [2.846, 1.865, -1.154, 0.659, 2.714, -0.116, -2.326]
-    # joint_states = [2.879, 1.786, -1.046, 0.65, 3.085, -0.29, -2.005]
-    # zip_object = zip(joint_states, js_orig)
-    # diff = []
-    # for list1_i, list2_i in zip_object:
-    #     diff.append(list1_i-list2_i)
-
+            # Slight changes in joints based on experience
+            if j in range(4):
+                diff.append(round(random.uniform(-0.2, 0.2), 3))
+            else:
+                diff.append(round(random.uniform(-0.5, 0.5), 3))
+        for j in range(joint_count):
+            joint_states.append(js_orig[j] + diff[j])    
+    i += 1
 
     if SET_LAST:
         joint_states[-1] = 0
     goal_pos = kn.calc_forw_kin(kn.calc_transform(DH_PARAMS), js_orig, all_dof=True)
     servo_joints = kn.servo_calcs(DH_PARAMS, goal_pos, joint_states, orientation=True, max_iter=500,
                                   set_last=SET_LAST)[0]
-    if servo_joints == -1:
-        success.append(0)
-        servo_list = [np.nan] * 7
-    else:
-        for j in range(joint_count):
-            if abs(servo_joints[j]) > sp.pi:
-                cycles = int(abs(servo_joints[j] / (2 * sp.pi)))
-                print(cycles, servo_joints[j])
-                servo_joints[j] -= np.sign(servo_joints[j]) * 2 * sp.pi.evalf() * (cycles + 1)
-        success.append(1)
-        servo_list = [round(item, 4) for sublist in servo_joints.tolist() for item in sublist]
-        # if not check_joint_limits(servo_joints):
-        #     servo_joints = kn.servo_calcs(DH_PARAMS, goal_pos, joint_states, orientation=True, max_iter=500,
-        #                           set_last=SET_LAST, joint_limits=True)[0]
-        #     # TODO: copy checks
+
+    servo_list = process_result(servo_joints, success)
+        
+    if not check_joint_limits(servo_joints):
+        success = [-1]
+        print('Exceeded limits, runnning with new configuration')
+        with open('test.csv', 'a', encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(js_orig + joint_states + servo_list + success)
+        success = []
+        servo_joints = kn.servo_calcs(DH_PARAMS, goal_pos, joint_states, orientation=True, max_iter=500,
+                                      set_last=SET_LAST, joint_limits=True)[0]
+        servo_list = process_result(servo_joints, success)
+
+    if servo_joints != -1:
         diff_mod = sp.Matrix(joint_states).transpose() - servo_joints
+
         # Ignore last joint in evaluation of solution, as that is 'unknown' due to camera precision issue
         if (SET_LAST):
             diff = sp.Matrix(diff[:6])
