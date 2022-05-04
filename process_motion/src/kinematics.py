27fb5d91154a57ff43813649c1258fd86b868e90
Aron Svastits <svastits.aron@gmail.com>
2022-05-04 11:44:22 +0200

    remove unnecessary all_dof parameter + flake8

--- process_motion/src/kinematics.py
+++ process_motion/src/kinematics.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
-"""
-Kinematics calculations of a robot
+"""Kinematics calculations of a robot."""
 
-    Denavit-Hartenberg parameters:
-"""
-
-import math
 import csv
+import math
+
 import numpy as np
 import sympy as sp
 
@@ -17,10 +14,12 @@ UPPER_LIMITS = [170, 120, 170, 120, 170, 120, 175]
 LOWER_LIMITS_R = [0.9 * math.radians(deg) for deg in LOWER_LIMITS]
 UPPER_LIMITS_R = [0.9 * math.radians(deg) for deg in UPPER_LIMITS]
 
+
 def denavit_to_matrix(s_a, s_alpha, s_d, s_theta, tool_length=0):
     """
-    Calculates homogenous tranformation matrix from Denavit-Hartenberg parameters
-        (Khalil-modification)
+    Calculate homogenous tranformation matrix from DH parameters.
+
+    Using the Denavit-Hartenberg convention by Khalil
     """
     a = sp.parsing.parse_expr(s_a)
     alpha = sp.parsing.parse_expr(s_alpha)
@@ -28,21 +27,22 @@ def denavit_to_matrix(s_a, s_alpha, s_d, s_theta, tool_length=0):
     theta = sp.parsing.parse_expr(s_theta)
 
     T = sp.Matrix([[sp.cos(theta), -sp.sin(theta), 0, a],
-                 [sp.sin(theta) * sp.cos(alpha), sp.cos(theta) * sp.cos(alpha), -sp.sin(alpha),
+                  [sp.sin(theta) * sp.cos(alpha), sp.cos(theta) * sp.cos(alpha), -sp.sin(alpha),
                   -d * sp.sin(alpha)],
-                 [sp.sin(theta) * sp.sin(alpha), sp.cos(theta) * sp.sin(alpha), sp.cos(alpha),
-                  d * sp.cos(alpha)],
-                 [0, 0, 0, 1]])
+                  [sp.sin(theta) * sp.sin(alpha), sp.cos(theta) * sp.sin(alpha), sp.cos(alpha),
+                   d * sp.cos(alpha)],
+                  [0, 0, 0, 1]])
     return T
+
 
 def calc_jacobian(dh_params, joint_pos=None, exceeded=None):
     """
-    Calculates the Jacobian matrix for a robot chain from base to end effector based on
-        Denavit-Hartenberg parameters
+    Calculate the Jacobian matrix for a robot chain from base to end effector.
+
+        - param dh_params: Denavit-Hartenberg parameters of the robot
         - param exceeded: if a joint limit is exceeded by exactly one joint, the specified joint is
             set as constant in the jacobian calculations (joint_pos must not be None!)
     """
-
     joint_count = 0
     while f'joint_{joint_count + 1}' in dh_params.keys():
         joint_count += 1
@@ -55,10 +55,10 @@ def calc_jacobian(dh_params, joint_pos=None, exceeded=None):
     abs_pos = trans_matrix[:3, 3]
 
     rot_matrix = trans_matrix[:3, :3]
-    c_p = sp.sqrt(rot_matrix[0,0]**2 + rot_matrix[1,0]**2) # this equals cos(pitch)
-    yaw = sp.atan2(rot_matrix[1,0], rot_matrix[0,0])
-    pitch = sp.atan2(-rot_matrix[2,0], c_p)
-    roll = sp.atan2(rot_matrix[2,1], rot_matrix[2,2])
+    c_p = sp.sqrt(rot_matrix[0, 0]**2 + rot_matrix[1, 0]**2)  # this equals cos(pitch)
+    yaw = sp.atan2(rot_matrix[1, 0], rot_matrix[0, 0])
+    pitch = sp.atan2(-rot_matrix[2, 0], c_p)
+    roll = sp.atan2(rot_matrix[2, 1], rot_matrix[2, 2])
 
     if joint_pos is None:
         Jv = abs_pos.jacobian(q_symbols)
@@ -90,8 +90,8 @@ def calc_jacobian(dh_params, joint_pos=None, exceeded=None):
     Jw = sp.Matrix([roll, pitch, yaw]).jacobian(q_symbols)
     J = sp.Matrix([Jv, Jw])
 
-    if  c_p.subs(zip(q_symbols, joint_pos)).evalf() < 1e-6:
-        print("Pitch is almost +-90°")
+    if c_p.subs(zip(q_symbols, joint_pos)).evalf() < 1e-6:
+        print('Pitch is almost +-90°')
         # in this case, the solution is not straighforward
         # atan2(0, 0) would be nan -> orientation is calculated with a different joint setup
         # only the first (from TCP) joint is modified, that changes pitch angle slightly
@@ -107,9 +107,7 @@ def calc_jacobian(dh_params, joint_pos=None, exceeded=None):
 
 
 def pseudo_inverse(J):
-    """
-    Calculates the pseudo inverse for a given Jacobian matrix
-    """
+    """Calculate the pseudo inverse for a given Jacobian matrix."""
     det = (J * J.transpose()).det()
     if det < 1e-9:
         print(f'Singularity reached, determinant: {det}')
@@ -118,13 +116,12 @@ def pseudo_inverse(J):
     J_inv = J.transpose() * (J * J.transpose()).inv()
     return J_inv
 
+
 def pseudo_inverse_svd(J):
-    """
-    Calculates the pseudo inverse for a given Jacobian matrix based on SVD
-    """
+    """Calculate the pseudo inverse for a given Jacobian matrix based on SVD."""
     U, S, V = np.linalg.svd(np.array(J).astype(np.float64))
     if np.linalg.det(np.diag(S)) < 1e-9:
-        print("Singularity reached")
+        print('Singularity reached')
         return -1
     S_inv = np.linalg.inv(np.diag(S))
     columns_to_add = V.shape[0] - S.shape[0]
@@ -136,35 +133,32 @@ def pseudo_inverse_svd(J):
 
     return sp.Matrix(J_inv)
 
+
 def damped_least_squares(J, mu):
-    """
-    Calculates matrix for servoing with damped least squares method
-    """
+    """Calculate matrix for servoing with damped least squares method."""
     J_inv = J.transpose() * (J * J.transpose() + mu * np.eye(sp.shape(J)[0])).inv()
     return J_inv
 
 
 def check_joint_limits(joint_states):
-    """
-    Returns, whether the given joint states are within the limits
-    """
+    """Return, whether the given joint states are within the limits."""
     exceeded = []
-    if len(joint_states) != len (LOWER_LIMITS):
+    if len(joint_states) != len(LOWER_LIMITS):
         print('Limits are invalid for this robot')
         return False, exceeded
     for i in range(len(joint_states)):
-        if (joint_states[i] < LOWER_LIMITS_R[i] 
-            or joint_states[i] > UPPER_LIMITS_R[i]):
-            print (f'Limits exceeded by joint {i + 1}')
+        if (joint_states[i] < LOWER_LIMITS_R[i] or joint_states[i] > UPPER_LIMITS_R[i]):
+            print(f'Limits exceeded by joint {i + 1}')
             exceeded.append(i + 1)
-        valid = True if len(exceeded) == 0 else False
+        valid = len(exceeded) == 0
     return valid, exceeded
 
 
 def servo_calcs(dh_params, goal_pos, joint_states, max_iter=500, pos_tol=1e-5,
-                rot_tol=1e-3, set_last = 0, joint_limits = 0):
+                rot_tol=1e-3, set_last=0, joint_limits=0):
     """
-    Calculates the joint states for a given cartesian position with servoing in cartesian space
+    Calculate the joint states for a given cartesian position with servoing in cartesian space.
+
         - param goal_pos: target cartesian position with roll-pitch-yaw orientation
         - param joint_states: actual joint values
         - max_iter: maximum number of iterations
@@ -176,14 +170,12 @@ def servo_calcs(dh_params, goal_pos, joint_states, max_iter=500, pos_tol=1e-5,
             - 1 means to enforce limits during the iterations (exclude joint from Jacobian)
             - 2 means to calculate with additional goal vector
             - 3 means to enforce limits during the iterations with additionan goal vector
-            
-
     """
     goal_pos = sp.Matrix(goal_pos).transpose()
     start_joints = joint_states
     joint_count = len(joint_states)
 
-    with open('log.csv', 'w', encoding="utf-8") as file:
+    with open('log.csv', 'w', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow([f'joint{i + 1}' for i in range(joint_count)])
 
@@ -201,8 +193,9 @@ def servo_calcs(dh_params, goal_pos, joint_states, max_iter=500, pos_tol=1e-5,
     #     print("[ERROR] Initial distance too big")
     #     return None, None
     blocked = False
-    while ((diff[:3,:].norm() > pos_tol or diff[3:,:].norm() > rot_tol or blocked) and i < max_iter):
-        print(diff[:3,:].norm(), diff[3:,:].norm().evalf(), i)
+    while ((diff[:3, :].norm() > pos_tol or diff[3:, :].norm() > rot_tol or blocked)
+           and i < max_iter):
+        print(diff[:3, :].norm(), diff[3:, :].norm().evalf(), i)
         i += 1
         J = calc_jacobian(dh_params, joint_states)
         J_inv = pseudo_inverse_svd(J)
@@ -263,12 +256,12 @@ def servo_calcs(dh_params, goal_pos, joint_states, max_iter=500, pos_tol=1e-5,
         new_joints = sp.Matrix(joint_states) + delta_theta
         sp.pprint(new_joints.transpose().evalf(3))
 
-        with open('log.csv', 'a', encoding="utf-8") as file:
+        with open('log.csv', 'a', encoding='utf-8') as file:
             writer = csv.writer(file)
             writer.writerow(new_joints.evalf(5))
 
         joint_states = [item for sublist in new_joints.tolist() for item in sublist]
-        
+
         # Limits are enforced, but new delta_theta in reduced DOF scenario can exceed a limit
         #   for another joint
         if joint_limits in [1, 3]:
@@ -288,43 +281,41 @@ def servo_calcs(dh_params, goal_pos, joint_states, max_iter=500, pos_tol=1e-5,
         else:
             blocked = False
     if i == max_iter:
-        print("[ERROR] Could not reach target position in given iterations")
+        print('[ERROR] Could not reach target position in given iterations')
         return None, None
     return sp.Matrix([joint_states]).evalf(4), diff.evalf(4)
 
-def servo_all_methods(dh_params, goal_pos, joint_states, set_last = 0):
-    """
-    Runs servo_calcs with 3 different methods (if necessary) to avoid joint limits
-    """
+
+def servo_all_methods(dh_params, goal_pos, joint_states, set_last=0):
+    """Run servo_calcs with 3 different methods (if necessary) to avoid joint limits."""
     j_s = joint_states.copy()
     servo_joints = servo_calcs(dh_params, goal_pos, j_s, set_last=set_last)[0]
     process_result(servo_joints)
 
     # Check joint limits and re-run test, if they were exceeded
-    if servo_joints != None and not check_joint_limits(servo_joints)[0]:
-        print('[WARNING] Exceeded limits, runnning with new configuration')        
+    if servo_joints is not None and not check_joint_limits(servo_joints)[0]:
+        print('[WARNING] Exceeded limits, runnning with new configuration')
         servo_joints = servo_calcs(dh_params, goal_pos, j_s, set_last=set_last,
-                                      max_iter = 250, joint_limits=1)[0]
-        if servo_joints == None:
+                                   max_iter=250, joint_limits=1)[0]
+        if servo_joints is None:
             print('[WARNING] Enforcing joint limits also failed, retrying with goal vector')
             servo_joints = servo_calcs(dh_params, goal_pos, j_s, set_last=set_last,
-                                          max_iter = 250, joint_limits=2)[0]
-            if servo_joints == None:
+                                       max_iter=250, joint_limits=2)[0]
+            if servo_joints is None:
                 print('[WARNING] Goal vector failed, last attempt with enforcing + goal vector')
                 servo_joints = servo_calcs(dh_params, goal_pos, j_s, set_last=set_last,
-                                              max_iter = 250, joint_limits=3)[0]
+                                           max_iter=250, joint_limits=3)[0]
         process_result(servo_joints)
-        if servo_joints != None and not check_joint_limits(servo_joints)[0]:
+        if servo_joints is not None and not check_joint_limits(servo_joints)[0]:
             print('[ERROR] New configuration was also not successful')
             servo_joints = None
     if servo_joints is not None:
         print(f'[INFO] Found valid solution: {servo_joints}')
     return servo_joints
 
+
 def process_result(joint_result):
-    """
-    Checks for joint values that can be wrapped around
-    """
+    """Check for joint values that can be wrapped around."""
     if joint_result is not None:
         for i in range(len(joint_result.tolist())):
             if abs(joint_result[i]) > sp.pi:
@@ -335,11 +326,9 @@ def process_result(joint_result):
         return processed_js
     return None
 
-def calc_transform(dh_params):
-    """
-    Calculates base to end effector transform
-    """
 
+def calc_transform(dh_params):
+    """Calculate base to end effector transform."""
     joint_count = 0
     while f'joint_{joint_count + 1}' in dh_params.keys():
         joint_count += 1
@@ -358,25 +347,25 @@ def calc_transform(dh_params):
     return trans_matrix
 
 
-def calc_forw_kin(T, joint_pos, all_dof=False):
+def calc_forw_kin(T, joint_pos):
     """
-    Calculates cartesian position and orientation from the transormation matrix
-        based on given joint positions
-    """
+    Calculate cartesian position and orientation.
 
+        - param T: transformation matrix (parametric)
+    """
     joint_count = len(joint_pos)
 
     q_symbols = [sp.symbols(f'q{i + 1}') for i in range(joint_count)]
     abs_pos = T[:3, 3]
     R = T[:3, :3]
 
-    c_p = sp.sqrt(R[0,0]**2 + R[1,0]**2) # this equals cos(pitch)
-    roll = sp.atan2(R[2,1], R[2,2])
-    pitch = sp.atan2(-R[2,0], c_p)
-    yaw = sp.atan2(R[1,0], R[0,0])
+    c_p = sp.sqrt(R[0, 0]**2 + R[1, 0]**2)  # this equals cos(pitch)
+    roll = sp.atan2(R[2, 1], R[2, 2])
+    pitch = sp.atan2(-R[2, 0], c_p)
+    yaw = sp.atan2(R[1, 0], R[0, 0])
 
     if c_p.subs(zip(q_symbols, joint_pos)).evalf() < 1e-6:
-        print("Pitch is almost +-90°")
+        print('Pitch is almost +-90°')
         # in this case, the RPY angles are not straighforward
         #   roll and yaw angles axis are the same in opposite directions
         #   atan2(0, 0) would be nan
@@ -390,19 +379,21 @@ def calc_forw_kin(T, joint_pos, all_dof=False):
             else:
                 joint_pos = j_s.copy()
                 break
-    if not all_dof and abs(abs(pitch.subs(zip(q_symbols, joint_pos))).evalf() - sp.pi/2) < 0.05:
+    if abs(abs(pitch.subs(zip(q_symbols, joint_pos))).evalf() - sp.pi/2) < 0.05:
         return sp.Matrix([abs_pos, roll-yaw, pitch]).subs(zip(q_symbols, joint_pos)).evalf()
 
     return sp.Matrix([abs_pos, roll, pitch, yaw]).subs(zip(q_symbols, joint_pos)).evalf()
 
+
 def adjust_goal_pos(trans_matrix, joint_states, goal_pos, tries=1):
     """
-    Adjusts the goal position based on the current joint states:
+    Adjust the goal position based on the current joint states.
+
         1. By transition -180° <-> 180° add +- 360° to orientation permanently
         2. If pitch is near 90° -> reduce DOF-s temporarily
             -> two goal_pos variables must be returned (permanent and temporary)
-    Besides, can set the 7th joint of the robot so, that the resulting position is the 'closest'
-        to the goal position
+        Besides, can set the 7th joint of the robot so, that the resulting position
+            is the 'closest' to the goal position
         - param tries: odd integer, meaning the number of evenly distributed values for joint7
             to try out, tries = 1 means joint states are not modified
         - returns the the two adjusted goal positions and the adjusted joint states
@@ -411,7 +402,7 @@ def adjust_goal_pos(trans_matrix, joint_states, goal_pos, tries=1):
     js_tmp = joint_states.copy()
     goal_pos_i = goal_pos.copy()
     tries = max(tries, 1)
-    if tries %2 == 0:
+    if tries % 2 == 0:
         tries += 1
     for i in range(tries):
         # create tries+1 intervals and iterate through split points with last joint
@@ -448,4 +439,3 @@ def adjust_goal_pos(trans_matrix, joint_states, goal_pos, tries=1):
     if tries > 1:
         print(f'closest js: {joint_states}')
     return goal_pos_tmp
-
